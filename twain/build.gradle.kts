735a297a68ad776ce7446000360386f96534bdf8
Colin Lee <mncolinlee@gmail.com>
2023-04-03 16:02:41 -0500

    Add publishing and signing configuration

--- twain/build.gradle.kts
+++ twain/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 @Suppress(
     "DSL_SCOPE_VIOLATION"
 ) // ignore warnings here, it's an Android Studio bug: https://youtrack.jetbrains.com/issue/KTIJ-19369
@@ -5,6 +7,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -23,7 +26,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
@@ -45,18 +48,92 @@ android {
     }
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "com.meetup"
-            artifactId = "twain"
-            version = "1.0"
+val version = project.properties["VERSION_NAME"].toString()
+val group = project.properties["GROUP"].toString()
 
-            afterEvaluate {
-                from(components["release"])
+fun isReleaseBuild(): Boolean {
+    return !project.properties["VERSION_NAME"].toString().contains("SNAPSHOT")
+}
+
+fun getReleaseRepositoryUrl(): URI {
+    val url =
+        if (project.hasProperty("RELEASE_REPOSITORY_URL")) project.properties["RELEASE_REPOSITORY_URL"].toString()
+        else "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+    return URI(url)
+}
+
+fun getSnapshotRepositoryUrl(): URI {
+    val url =
+        if (project.hasProperty("SNAPSHOT_REPOSITORY_URL")) project.properties["SNAPSHOT_REPOSITORY_URL"].toString()
+        else "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+    return URI(url)
+}
+
+fun getRepositoryUsername(): String {
+    return if (project.hasProperty("NEXUS_USERNAME")) project.properties["NEXUS_USERNAME"].toString()
+    else error("Needs username for repository")
+}
+
+fun getRepositoryPassword(): String {
+    return if (project.hasProperty("NEXUS_PASSWORD")) project.properties["NEXUS_PASSWORD"].toString()
+    else error("Needs password for repository")
+}
+
+publishing {
+    repositories {
+        maven {
+            val releasesRepoUrl =
+                project.properties["RELEASE_REPOSITORY_URL"]?.let { URI(it.toString()) }
+                    ?: error("Release repository not defined")
+            val snapshotsRepoUrl =
+                project.properties["SNAPSHOT_REPOSITORY_URL"]?.let { URI(it.toString()) }
+                    ?: error("Snapshot repository not defined")
+            url = (if (isReleaseBuild()) releasesRepoUrl else snapshotsRepoUrl)
+
+            credentials {
+                username = getRepositoryUsername()
+                password = getRepositoryPassword()
             }
         }
     }
+    publications {
+        create<MavenPublication>("twain") {
+            pom {
+                name.set(project.properties["POM_NAME"].toString())
+                description.set(project.properties["POM_DESCRIPTION"].toString())
+                url.set(project.properties["POM_URL"].toString())
+                groupId = project.properties["GROUP_ID"].toString()
+                artifactId = project.properties["ARTIFACT_ID"].toString()
+                version = project.properties["VERSION_NAME"].toString()
+
+                licenses {
+                    license {
+                        name.set(project.properties["POM_LICENCE_NAME"].toString())
+                        url.set(project.properties["POM_LICENCE_URL"].toString())
+                        distribution.set(project.properties["POM_LICENCE_DIST"].toString())
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set(project.properties["POM_DEVELOPER_ID"].toString())
+                        name.set(project.properties["POM_DEVELOPER_NAME"].toString())
+                        email.set(project.properties["POM_DEVELOPER_EMAIL"].toString())
+                    }
+                }
+
+                scm {
+                    connection.set(project.properties["POM_SCM_CONNECTION"].toString())
+                    developerConnection.set(project.properties["POM_SCM_DEV_CONNECTION"].toString())
+                    url.set(project.properties["POM_SCM_URL"].toString())
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["twain"])
 }
 
 dependencies {
